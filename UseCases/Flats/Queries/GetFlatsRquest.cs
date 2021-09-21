49305b181c5c4c1e468566554b270bc4ed33349d
timofey.klimov@inbox.ru <timofey.klimov@inbox.ru>
2021-09-21 11:25:27 +0300

    test

---
+++ UseCases/Flats/Queries/GetFlatsRquest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.User.Base;
+
+namespace UseCases.Flats.Queries
+{
+    public class GetFlatsRquest : BaseUserRequest<string>
+    {
+        public GetFlatsRquest(long chatId)
+            : base(chatId)
+        {
+
+        }
+    }
+}
---
+++ WepApp/Controllers/FlatsController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using UseCases.Flats.Queries;
+using WepApp.Dto;
+
+namespace WepApp.Controllers
+{
+    [Route("api/flats")]
+    public class FlatsController : ControllerBase
+    {
+        private IMediator _mediator;
+
+        public FlatsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{chatId}")]
+        public async Task<ApiResponse<string>> GetFlats(long chatId)
+        {
+            var result = await _mediator.Send(new GetFlatsRquest(chatId));
+
+            return ApiResponse<string>.Success(result);
+        }
+    }
+}
